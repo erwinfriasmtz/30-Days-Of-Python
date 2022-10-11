8049eee061efac4da945ad363f9b09f1fc6d1b8a
Erwin Alan Frias Martinez <alan.frias@erwcode.com>
2022-10-10 19:29:41 -0500

    :sparkles: (feat): day one python

---
+++ day_1/helloworld.py
@@ -0,0 +1,28 @@
+# Check the python version you are using.
+import numpy as np
+print('python3 --version')
+
+# Do the following operations.
+print(2 + 3)             # addition(+)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
+
+# Write strings on the python interactive shell.
+print('Erwin Alan')
+print('Hugo de Jesus')
+print('Mexico')
+print('I am enjoying 30 days of python')
+
+# Checking data types
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name': 'Oscar'}))  # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
