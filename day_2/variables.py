eb8439f73c2dddaee16e55dc26a431794ca4062e
Erwin Alan Frias Martinez <alan.frias@erwcode.com>
2022-10-11 20:50:29 -0500

    :sparkles: (feat): day two python

---
+++ day_2/variables.py
@@ -0,0 +1,53 @@
+# Day 2: 30 Days of python programming
+
+first_name = 'Erwin Alan'
+last_name = 'Frias Martinez'
+full_name = 'Erwin Alan Frias Martinez'
+country = 'México'
+city = 'Ciudad de México'
+age = 28
+year = 2022
+is_married = True
+is_true = True
+is_light_on = False
+color, size, language, phone = 'Blue', 50, 'Español', 2381521477
+
+type(first_name)
+type(last_name)
+type(full_name)
+type(country)
+type(city)
+type(age)
+type(year)
+type(is_married)
+type(is_true)
+type(is_light_on)
+
+len(first_name)
+len(last_name)
+
+num_one, num_two = 5, 4
+total = num_one + num_two
+diff = num_two - num_one
+product = num_two * num_one
+division = num_two / num_one
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+area_of_circle = 3.1416 * 30 ** 2
+circum_of_circle = 3.1416 * 60
+radius = int(input('Radius:'))
+area_of_circle = 3.1416 * radius ** 2
+print(area_of_circle)
+
+first_name = input('First Name: ')
+last_name = input('Last Name: ')
+country = input('Country: ')
+age = input('Age: ')
+
+print('Full Name:', first_name, last_name)
+print('Country:', country)
+print('Age:', age)
+
+help('keywords')
